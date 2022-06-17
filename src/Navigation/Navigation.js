8316467f5b586a6b71d5553de44e01a86c06c15c
blackpearl15 <yashanand2477@gmail.com>
2022-06-17 21:19:38 +0530

    appoitntment

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -65,7 +65,7 @@ export default function Navigation() {
                 marginTop: "11px",
               }}
             >
-              Schedule
+              Appointments
             </Link>
           </li>
 
