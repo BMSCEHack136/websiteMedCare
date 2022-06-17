aa23430b5b401eb337765da59c6215ea8a310ce4
Diyabanerjee <diya.dbanerjee@gmail.com>
2022-06-17 21:39:27 +0530

    footer

---
+++ src/pages/Home/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Footer/Footer.css";
+// export default function Footer() {
+//   return <div><div className="footer">
+
+const Footer = () => (
+  <footer className="footer">
+    <p>@MedCare</p>
+  
+  </footer>
+  );
+  
+  export default Footer;
+
--- src/pages/Schedule/Schedule.js
+++ src/pages/Schedule/Schedule.js
@@ -1,20 +1,73 @@
+import { adaptV4Theme, Button } from "@mui/material";
 import React, { useState } from "react";
 import Login from "../../Common/Login";
 import { GlobalContext } from "../../GlobalData";
 // import "../Beds/style.css";
+import "../Schedule/Schedule.css";
 
 export default function Schedule() {
-  const { HospitalId} = React.useContext(GlobalContext)
+  const { HospitalId } = React.useContext(GlobalContext);
 
-  if ( HospitalId === "") return <Login/>
+  const [appt, setAppt] = React.useState("null");
 
-  else{
-    return(
-
-      <div>
-        schedule
-      </div>
-    )
+  async function fetchUrl() {
+    let res = await fetch(
+      `https://exlfit6t23.execute-api.us-east-1.amazonaws.com/ver1/app-log?id=${HospitalId}`
+    );
+    let data = await res.json();
+    setAppt(data);
   }
 
+  React.useEffect(() => {
+    fetchUrl();
+  }, [HospitalId]);
+
+  if (HospitalId === "") return <Login />;
+  else if (appt == "null") return <div>loading</div>;
+  else {
+    console.log(appt["data"], "hii");
+    return (
+      <div class="card">
+        <h1 style={{ fontSize: "50px" }}>Appointments</h1>
+        <hr style={{ marginBottom: "5%" }} />
+        <h1 style={{}}>Upcoming Appointments</h1>
+        <div class="card2">
+          <table class="content-table">
+            <thead>
+              <tr>
+                <th>Patient_id</th>
+                <th>Name</th>
+                <th>Date/Time</th>
+                <th>Phone</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {appt["data"].map((row) => {
+                return <>{row.resolve === "0" && <Row data={row} />}</>;
+                // console.log(row,'hiii')
+              })}
+            </tbody>
+          </table>
+        </div>
+       
+      </div>
+    );
+  }
 }
+
+const Row = ({ data }) => {
+  return (
+    <tr>
+      <td>{data.Patient_id}</td>
+      <td>-</td>
+      <td>
+        {data.Date} {data.Time}
+      </td>
+      <td>-</td>
+      <td>
+        <Button type="button">Pending</Button>
+      </td>
+    </tr>
+  );
+};
